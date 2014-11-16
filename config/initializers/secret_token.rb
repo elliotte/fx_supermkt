b577f3a8545c512f9a0d2e4339e974b7ce88d899
--get <m_elliott@live.com>
2014-11-16 12:01:05 +0000

    quick setup for H

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fxsupermarket::Application.config.secret_key_base = '4ec6610ed4bb2f1282b2856118bca2433301f7636dbe0154a334ad38aafe6e3c562ba3ac691a3512e154c9cfddd39e8b5ad264ee13ab074202a1f7377e5d2bcf'
